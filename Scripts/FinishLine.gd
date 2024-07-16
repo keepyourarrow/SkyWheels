91661c4c65c904f649096185ef0b29cfadace51a
EternusCor <eternuscor@gmail.com>
2024-07-16 21:35:15 +0530

    Level-updates- and - signs

--- Scripts/FinishLine.gd
+++ Scripts/FinishLine.gd
@@ -5,13 +5,14 @@ func _on_area_2d_area_entered(area):
 		switch_level()
 
 func switch_level():
-	var current_level = get_tree().current_scene.name
+	var current_level = str(get_tree().current_scene.name)
 	var next_level
 	
-	for c in "level_1":
+	for c in current_level:
 		if c <= "9" and c >= "0":
 			var num = int(c) + 1
 			num = str(num)
-			next_level = "res://Scenes/level_" + num + ".tscn"
+			next_level = "res://Scenes/levels/level_" + num + ".tscn"
+			get_tree().change_scene_to_file(next_level)
 	
-	get_tree().change_scene_to_file(next_level)
+	
